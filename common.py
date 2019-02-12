ae891a2f31a498b488eeec708606a9019dcfe11f
Matthias Bock <mail@matthiasbock.net>
2019-02-13 00:19:56 +0100

    Added some explanatory text

--- common.py
+++ common.py
@@ -6,9 +6,15 @@ BitsPerProductTerm = 88
 
 
 def generateTablesForProductTerms(LABCount):
-    html = "Bits 0 through "+str(LABCount*MacrocellsPerLAB*ProductTermsPerMacrocell*BitsPerProductTerm-1)+" configure the product terms.\n<br/><br/>\n"
 
-    html += "<table id=\"product-terms\">\n<tbody>\n"
+    html = "The device contains "+str(LABCount)+" Logic Array Blocks (LABs), each containing 16 macrocells.<br/>\n"
+    html += "Bits 0 through "+str(LABCount*MacrocellsPerLAB*ProductTermsPerMacrocell*BitsPerProductTerm-1)+" of the bitstream configure all of the device's product terms.<br/>\n"
+    html += "Each product term is configured by "+str(BitsPerProductTerm)+" bits.\n"
+    html += "A '1' represents a disregarded signal, a '0' appends the signal to the product.<br/>\n"
+    html += "The lower 72 bits switch global signals, routed to the LAB from the PIA (36 signals, inverted and non-inverted).<br/>\n"
+    html += "The higher 16 bits switch regional foldback signals, i.e. outputs from macrocells of the same LAB.<br>\n" 
+
+    html += "\n<br/>\n<table id=\"product-terms\">\n<tbody>\n"
 
     offset = 0
 
--- epm7032s.py
+++ epm7032s.py
@@ -22,12 +22,32 @@ This is an automatically generated page.
 The corresponding generator can be found <a href="https://github.com/programmable-logic-tools/altera-max7000-db">here</a>.
 The raw bitstream might have to be extracted from an Altera Quartus POF file first.
 <a href="https://github.com/programmable-logic-tools/libalterapof/tree/master/doc">libalterpof</a> may help with that.
+<br/><br/>
+The bitstream for the <a href="https://www.intel.com/content/dam/www/programmable/us/en/pdfs/literature/ds/archives/m7000.pdf">EPM7032S</a> consists of """+str(bitCount)+""" bits in total.<br/>
 <h2>Product terms</h2>
 """
 
 html += generateTablesForProductTerms(
             LABCount = LABCount
             )
+
+html += """
+<h2>Remaining bits</h2>
+The remaining bits of the bitstream are expected to configure:
+<ul>
+<li>PIA-to-LAB routing</li>
+<li>Product selection for sum-of-products term</li>
+<li>XOR second input signal selection</li>
+<li>Cascading from previous/to next macrocell</li>
+<li>Flipflop vs. latch selection/mux</li>
+<li>FF clock signal selection</li>
+<li>FF enable signal selection</li> 
+<li>I/O Output Enable (OE) signal selection</li>
+<li>I/O type selection: push-pull vs. open collector</li>
+<li>I/O slew rate control: slow vs. fast</li>
+<li>JTAG enable/disable</li>
+</ul>
+"""
 
 html += "</body>\n</html>"
 
