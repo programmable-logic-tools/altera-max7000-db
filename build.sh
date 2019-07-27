f7b696232ea40be98179a88c96170bf1dfc64010
Matthias Bock <mail@matthiasbock.net>
2019-07-27 14:49:59 +0200

    Minor changes

--- build.sh
+++ build.sh
@@ -9,14 +9,17 @@ TARGET_FOLDER="../altera-max7000-db.gh-pages"
 # Clear the target folder
 here=$(pwd)
 cd "$TARGET_FOLDER"
-rm -f *.html *.css
+rm -f epm*.html *.css
+git pull
 cd "$here"
 
 # Generate new content
+echo "Generating HTML..."
 ./epm7032s.py
 
 # Copy content to target folder
-cp -v *.html *.css "$TARGET_FOLDER/"
+echo "Copying output to branch gh-pages..."
+cp -v epm*.html *.css "$TARGET_FOLDER/"
 
 # Create a git commit
 hash=$(git rev-parse --short HEAD)
